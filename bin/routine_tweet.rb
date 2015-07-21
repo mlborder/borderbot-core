34f883703cf8aba4de506c4b456c2e4fe68f44e7
treby <treby@atelier-nodoka.net>
2015-07-21 17:08:47 +0900

    Add progress rate.

--- bin/routine_tweet.rb
+++ bin/routine_tweet.rb
@@ -19,7 +19,18 @@ current_data.select{|key, value| key.include?('border_') && !value.nil? }
 end
 
 current_time = Time.at current_data['time']
-tweet_txt = "『#{Mlborder::Util.event_at(current_time)['name']}』\n#{current_time.strftime('%m/%d %H:%M')} #imas_ml\n"
+current_event = Mlborder::Util.event_at(current_time)
+progress = ''
+unless current_event['started_at'].nil?
+  start_time = Time.parse current_event['started_at']
+  end_time = Time.parse current_event['ended_at']
+
+  all_time = end_time.to_i - start_time.to_i
+  past_time = current_time.to_i - start_time.to_i
+  progress = "(#{(past_time.to_f / all_time * 100).round(1)}%)"
+end
+
+tweet_txt = "『#{current_event['name']}』\n#{current_time.strftime('%m/%d %H:%M')}#{progress} #imas_ml\n"
 rank_list = RBatch.common_config['MLBORDER_PRIZE_RANK_LIST']
 
 border_list.select{|border| rank_list.include? border[:rank]}.each do |border|
